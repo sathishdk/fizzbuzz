ac4a52060b82ad78bc894bf25bf44e68719d3efb
Sathish DK <sathishdk@sathishdk.com>
2015-06-11 12:49:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ app.controller("FizzBuzzCtrl", function($scope){
   
   $scope.output = [];
   
-  for (var i = $scope.firstNo; i <= $scope.secondNo; i++){
+  var fizzBuzz = function(){
+    for (var i = $scope.firstNo; i <= $scope.secondNo; i++){
     $scope.fizz = i%3;
     $scope.buzz = i%5;
     $scope.fizzBuzz = i%15;
-    if ($scope.fizz === 0) {
-      $scope.output.push(i);
+      if ($scope.fizz === 0) {
+        $scope.output.push(i);
+      }
+      else if ($scope.buzz === 0) {
+        $scope.output.push(i);
+      }
+      else if ($scope.fizzBuzz === 0) {
+        $scope.output.push(i);
+      }
     }
-    else if ($scope.buzz === 0) {
-      $scope.output.push(i);
-    }
-    else if ($scope.fizzBuzz === 0) {
-      $scope.output.push(i);
-    }
+    console.log($scope.output);
   }
-  
-  console.log($scope.output);
   
 });
 
